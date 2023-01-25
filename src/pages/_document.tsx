5bfa754afe8d8b5448bfa0f787ffec705928e7e1
matthiasposch <posch.matthias@gmail.com>
2023-01-25 10:16:04 +0100

    update pirsch

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
           {CssBaseline.flush()}
           <script defer type="text/javascript" src="https://api.pirsch.io/pirsch.js"
                   id="pirschjs"
-                  data-code="sc3in23S23tFlwTCNGOo99XG6mZVtrUL"></script>
+                  data-code="DRCSiiTXNvLfXgfgGc98XbgeZVGs2xmd"></script>
           <Script id="crisp" dangerouslySetInnerHTML={{ __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="31493b56-f455-4e23-803b-9e8e659712a6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();` }}></Script>
         </Head>
         <body>
